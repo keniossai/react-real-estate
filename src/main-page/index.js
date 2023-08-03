6884aaa3f4da73c2ef8caee461f65d6085b6e923
Ken Ossai <49806812+keniossai@users.noreply.github.com>
2023-08-03 15:44:50 +0100

    Update index.js

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,29 +1,37 @@
 import { useEffect, useState, useMemo } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./main.css";
 import Header from "./header";
+import FeaturedHouse from "./featured-house";
 
 function App() {
   const [allHouses, setAllHouses] = useState([]);
   useEffect(() => {
-    const fetchHouse = async () => {
+    const fetchHouses = async () => {
       const rsp = await fetch("/houses.json");
       const houses = await rsp.json();
       setAllHouses(houses);
-    }
-    fetchHouse();
-  }, [])
+    };
+    fetchHouses();
+  }, []);
 
   const featuredHouse = useMemo(() => {
     if (allHouses.length){
       const randomIndex = Math.floor(Math.random() * allHouses.length);
       return allHouses[randomIndex];
-    }
+    };
   }, [allHouses]);
     
+  // debugger
   return (
-    <div className="container">
-      <Header subtitle="Provide houses all over the world" title="Some title" />
-    </div>
+    <Router>
+      <div className="container">
+        <Header subtitle="Provide houses all over the world" />
+        <Routes>
+          <Route path="/" element={<FeaturedHouse house={featuredHouse} />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
