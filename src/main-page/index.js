32c6b898556cfeecacde4a7421eb6204d48783c5
Ken Ossai <49806812+keniossai@users.noreply.github.com>
2023-08-03 14:30:58 +0100

    Import useeffect and usestate

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,7 +1,16 @@
+import { useEffect, useState } from "react";
 import "./main.css";
 import Header from "./header";
 
 function App() {
+  useState([]);
+  useEffect(() => {
+    const fetchHouse = async () => {
+      const rsp = await fetch("/houses.json");
+      const houses = await rsp.json();
+    }
+    fetchHouse();
+  }, [])
   return (
     <div className="container">
       <Header subtitle="Provide houses all over the world" title="Some title" />
